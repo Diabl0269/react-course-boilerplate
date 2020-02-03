0d96adfd1ffab754a8492b46be3177a7557fbd38
Diabl0269 <talefronny@gmail.com>
2020-02-03 12:10:38 +0200

    init commit

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { Header } from "../../components/Header";
+
+let startLogout, wrapper;
+beforeEach(() => {
+  startLogout = jest.fn();
+  wrapper = shallow(<Header startLogout={startLogout} />);
+});
+
+test("should render Header correctly", () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test("should call startLogout on click", () => {
+  wrapper.find('button').prop('onClick')();
+  expect(startLogout).toHaveBeenLastCalledWith();
+});
---
+++ src/tests/setupTests.js
@@ -0,0 +1,9 @@
+import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
+import Enzyme from 'enzyme';
+
+DotEnv.config({ path : '.env.test' });
+
+Enzyme.configure({
+    adapter: new Adapter()
+});
